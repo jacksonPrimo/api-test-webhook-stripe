46e6a90cd960a8b4376dcf9f04648dde565fa0c3
jacksonPrimo <jackson.primo12@gmail.com>
2021-12-02 08:21:20 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(express.json())
+app.use(cors({ origin: process.env.CORS_ORIGIN || '*' }))
+app.get('/', (req, res)=> res.send('server running'))
+app.get('/webhook', (req, res)=> {
+  console.log("body=========>", req.body)
+})
+app.post('/webhook', (req, res)=> {
+  console.log("body=========>", req.body)
+})
+module.exports = app
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const app = require('./app')
+require('dotenv').config()
+const port = process.env.PORT || 3000
+app.listen(port, ()=>console.log(`server listening in port: ${port}`))
