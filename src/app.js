13ce3e7d815434ce9bdca009c1b3431721c12a87
jacksonPrimo <jackson.primo12@gmail.com>
2021-12-02 09:52:29 -0300

    more logs

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.post('/webhook', (req, res)=> {
 
 
   console.log("status with data=>object =========>", req.body.data.object.status)
+  console.log("plan with data=>object =========>", req.body.data.object.plan)
   console.log("subscription with data=>object =========>", req.body.data.object.id)
   console.log("customer with data=>object =========>", req.body.data.object.customer)
 
